59a9cb5b72c4d290dde7f0826e9da568521b49a2
K3I <80532241+sonkbhb00@users.noreply.github.com>
2025-03-07 00:15:39 +0700

    Delete textureManager.h

--- textureManager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "game.hpp"
-
-class textureManeger{
-public:
-    static SDL_Texture* loadTexture(const char* fileName);
-    static void draw(SDL_Texture *texture, SDL_Rect srcR, SDL_Rect destR);
-};
